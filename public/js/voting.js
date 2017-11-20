cb2b7d63df45d28a55d66d3d06567339ad998f5a
bels <bels11@yahoo.com>
2017-11-20 08:14:23 -0500

    Should in theory post

--- public/js/voting.js
+++ public/js/voting.js
@@ -2,7 +2,22 @@ $(function(){
 
 	$('#nominate-form').submit(function(e){
 		e.preventDefault();
+		var $form = $(this);
+		var submitData = {};
+		$('.office-group').each(function(index,element){
+			var $element = $(element);
+			var id = $element.find('.office-id').val();
+			var name = $element.find('.office-data').val();
+			submitData[id] = name;
+		});
 		
+		$.ajax({
+			method: $form.attr('method'),
+			url: $form.attr('action'),
+			data: submitData
+		}).done(function(){
+			
+		});
 	});
 
 });
